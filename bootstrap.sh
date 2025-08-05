0a4ace34174dbd03d5df9e2d0c5dd8881815d8c2
Andrew Luo <andrew@andluo.com>
2025-08-05 11:45:03 -0400

    mo shit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,4 @@
 ln -s ~/.config/.zshrc ~/.zshrc
+ln -s ~/.config/.p10k.zsh ~/.p10k.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
