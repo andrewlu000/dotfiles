b977c53807f49ff4ad735ceb6430bddb875f5fce
Andrew Luo <andrew@andluo.com>
2025-08-05 12:46:03 -0400

    Updated script for package installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # brew install viu
+
+REDHAT="$(cat /etc/redhat-release)"
+if [ "$REDHAT" == "Red Hat Enterprise Linux release 8.10 (Ootpa)" ]; then
+	echo "Installing useful packages!"
+#	sudo dnf install cargo 
+fi
